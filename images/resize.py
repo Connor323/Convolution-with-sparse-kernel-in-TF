4fae3a103e86be65728b3161f71efd23e6e803ec
Connor323 <haohanx323@outlook.com>
2017-10-19 17:25:52 -0400

    modify images

--- images/resize.py
+++ images/resize.py
@@ -5,5 +5,6 @@ files = glob.glob("*.png")
 
 for f in files:
     img = cv2.imread(f)
-    img = cv2.resize(img, (100, 200), interpolation=cv2.INTER_CUBIC)
+    h, w = img.shape[:2]
+    img = cv2.resize(img, (w/3*2, h/3*2), interpolation=cv2.INTER_CUBIC)
     cv2.imwrite(f, img)
